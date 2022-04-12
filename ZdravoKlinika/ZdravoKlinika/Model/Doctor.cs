0b1f0cb57778e07cb2627394c504c4e8655a9c8e
MiskLG <hornjak.ra139.2019@uns.ac.rs>
2022-04-12 15:15:57 -0700

    hotfix-path

---
+++ ZdravoKlinika/ZdravoKlinika/Model/Doctor.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System;
+
+public class Doctor : Employee
+{
+    private String specialty;
+    private String education;
+
+    public Doctor()
+    {
+
+    }
+
+    public string Specialty { get => specialty; set => specialty = value; }
+    public string Education { get => education; set => education = value; }
+
+    public bool IsSpecialist()
+    {
+        throw new NotImplementedException();
+    }
+
+}
