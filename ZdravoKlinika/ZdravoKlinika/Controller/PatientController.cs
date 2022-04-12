526618d71531c4fd95d9b598490d4b6cbdb6b18c
MiskLG <hornjak.ra139.2019@uns.ac.rs>
2022-04-12 09:23:12 -0700

    Added ser. and deser. for patients

--- ZdravoKlinika/ZdravoKlinika/Controller/PatientController.cs
+++ ZdravoKlinika/ZdravoKlinika/Controller/PatientController.cs
@@ -21,9 +21,10 @@ public class PatientController
         return patientService.GetById(id);
     }
 
-    public void CreatePatient(String personalId, String name, String lastname, DateTime dateOfBirth, Gender gender, String phone, String email, String password, String profilePicture, String parentName, BloodType bloodType, String occupation, String emergencyContactName, String emergencyContactPhone, List<String> alergies, List<String> diagnosis)
+    public void CreatePatient(String personalId, String name, String lastname, DateTime dateOfBirth, Gender gender, String phone, String email, String password, String profilePicture, Address address,String parentName, BloodType bloodType, String occupation, String emergencyContactName, String emergencyContactPhone, List<String> alergies, List<String> diagnosis)
     {
-        patientService.CreatePatient(personalId, name, lastname, dateOfBirth, gender, phone, email, password, profilePicture, parentName, bloodType, occupation, emergencyContactName, emergencyContactPhone, alergies, diagnosis);
+        Console.WriteLine("cont");
+        patientService.CreatePatient(personalId, name, lastname, dateOfBirth, gender, phone, email, password, profilePicture, address, parentName, bloodType, occupation, emergencyContactName, emergencyContactPhone, alergies, diagnosis);
     }
 
     public void UpdatePatient(String personalId, String name, String lastname, String phone, String email, String password, String profilePicture, String parentName, String occupation, String emergencyContactName, String emergencyContactPhone)
--- ZdravoKlinika/ZdravoKlinika/Data Handler/PatientDataHandler.cs
+++ ZdravoKlinika/ZdravoKlinika/Data Handler/PatientDataHandler.cs
@@ -1,20 +1,27 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 
 public class PatientDataHandler
 {
-    private static String fileLocation = "path";
+    private static String fileLocation = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + Path.DirectorySeparatorChar + "Resources"+ Path.DirectorySeparatorChar + "Data" + Path.DirectorySeparatorChar + "patients.json";
 
     public string FileLocation { get => fileLocation; set => fileLocation = value; }
 
     public void Write(List<Patient> patients)
     {
-        throw new NotImplementedException();
+    
+        JsonSerializerOptions options = new JsonSerializerOptions();
+        options.WriteIndented = true;
+        var json = JsonSerializer.Serialize(patients, options);
+        File.WriteAllText(fileLocation, json);
+
     }
 
     public List<Patient> Read()
     {
-        throw new NotImplementedException();
+        return JsonSerializer.Deserialize<List<Patient>>(File.ReadAllText(fileLocation));
     }
 
 }
