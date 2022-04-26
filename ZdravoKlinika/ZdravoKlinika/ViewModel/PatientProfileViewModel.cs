a4d0fe877c911f012f8fa2d064164554815e0ac6
mmg-lgm <matijamedic@hotmail.com>
2022-04-26 15:16:34 +0200

    ViewCreation continue
    Finished the looks of patient profile page and began creation of ViewModel

---
+++ ZdravoKlinika/ZdravoKlinika/ViewModel/PatientProfileViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZdravoKlinika.Model;
+
+namespace ZdravoKlinika.ViewModel
+{
+    internal class PatientProfileViewModel : INotifyPropertyChanged
+    {
+        private string name;
+        private string lastName;
+        private string parentName;
+        private string dateOfBirth;
+        private string gender;
+        private string phoneNumber;
+        private string email;
+        private string profilePictureLocation;
+        private string bloodType;
+        private string ocupation;
+        private string emergacyContact;
+        private string emergencyNumber;
+
+        private List<String> alergies;
+        private List<String> diagnoses;
+
+        //private List<PatientMedicationNotification> notifs;
+
+        public PatientProfileViewModel(MedicalRecord record)
+        {
+            //TODO
+            //this.name = record.
+            //etc etc
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
