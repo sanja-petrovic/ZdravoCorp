1d502869343bfed26188ac11830f249d7dcfc756
Dusan Jovanovic <jovanovic.ra144.2019@uns.ac.rs>
2022-04-28 21:23:15 +0200

    added UpravnikActivityDiagram and Move Class to ClassDiagram

---
+++ ZdravoKlinika/ZdravoKlinika/Model/Move.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+
+public class Move
+{
+    private String moveId;
+    private Room sourceRoom;
+    private Room destinationRoom;
+    private DateTime scheduledDateTime;
+
+    private System.Collections.Generic.List<Equipment> equipmentToMove;
+
+    public System.Collections.Generic.List<Equipment> EquipmentToMove
+    {
+        get
+        {
+            if (equipmentToMove == null)
+                equipmentToMove = new System.Collections.Generic.List<Equipment>();
+            return equipmentToMove;
+        }
+        set
+        {
+            RemoveAllEquipmentToMove();
+            if (value != null)
+            {
+                foreach (Equipment oEquipment in value)
+                    AddEquipmentToMove(oEquipment);
+            }
+        }
+    }
+
+    public void AddEquipmentToMove(Equipment newEquipment)
+    {
+        if (newEquipment == null)
+            return;
+        if (this.equipmentToMove == null)
+            this.equipmentToMove = new System.Collections.Generic.List<Equipment>();
+        if (!this.equipmentToMove.Contains(newEquipment))
+            this.equipmentToMove.Add(newEquipment);
+    }
+
+    public void RemoveEquipmentToMove(Equipment oldEquipment)
+    {
+        if (oldEquipment == null)
+            return;
+        if (this.equipmentToMove != null)
+            if (this.equipmentToMove.Contains(oldEquipment))
+                this.equipmentToMove.Remove(oldEquipment);
+    }
+
+    public void RemoveAllEquipmentToMove()
+    {
+        if (equipmentToMove != null)
+            equipmentToMove.Clear();
+    }
+
+}
