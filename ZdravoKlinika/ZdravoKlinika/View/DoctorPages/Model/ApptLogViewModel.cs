29926d75535bba507a7a0a9b8de85e52551e7e21
Sanya <sanjica.petrovic7@gmail.com>
2022-05-07 03:08:38 +0200

    Refactoring the log appointment dialog

---
+++ ZdravoKlinika/ZdravoKlinika/View/DoctorPages/Model/ApptLogViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZdravoKlinika.View.DoctorPages.Model
+{
+    public class ApptLogViewModel : ViewModelBase
+    {
+        public ObservableCollection<ITabViewModel> TabViewModels;
+        private ITabViewModel selectedViewModel;
+        public ApptLogViewModel()
+        {
+            TabViewModels = new ObservableCollection<ITabViewModel>();
+            TabViewModels.Add(new AnamnesisTab { Header = "Anamneza" });
+            TabViewModels.Add(new TherapyTab { Header = "Terapija" });
+            TabViewModels.Add(new ReferralTab { Header = "Uput za specijalistu" });
+            selectedViewModel = TabViewModels[0];
+        }
+
+        public ITabViewModel SelectedViewModel { get => selectedViewModel; set => SetProperty(ref selectedViewModel, value); }
+    }
+}
---
+++ ZdravoKlinika/ZdravoKlinika/View/DoctorPages/Model/TherapyTab.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZdravoKlinika.View.DoctorPages.Model
+{
+    public class TherapyTab : ViewModelBase, ITabViewModel
+    {
+        private string header;
+
+        public string Header { get => header; set => SetProperty(ref header, value); }
+    }
+}
