5c85a2707e8f9922562984a74911343d6fea240e
Dusan Jovanovic <jovanovic.ra144.2019@uns.ac.rs>
2022-05-17 23:10:25 +0200

    implemented renovation and equipment move windows

--- ZdravoKlinika/ZdravoKlinika/View/Manager/ViewModel/ManagerDoRenovationViewModel.cs
+++ ZdravoKlinika/ZdravoKlinika/View/Manager/ViewModel/ManagerDoRenovationViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace ZdravoKlinika.View.Manager.ViewModel
 {
     public class ManagerDoRenovationViewModel : ManagerViewModelBase
     {
+        private RoomController roomController;
+        private ObservableCollection<Room> rooms;
+
+        public ObservableCollection<Room> Rooms { get => rooms; set => rooms = value; }
+
+        public ManagerDoRenovationViewModel()
+        {
+            this.roomController = new RoomController();
+            Rooms = new ObservableCollection<Room>(this.roomController.GetRenovatableRooms());
+        }
     }
 }
--- ZdravoKlinika/ZdravoKlinika/View/Manager/ViewModel/ManagerTransferEquipmentViewModel.cs
+++ ZdravoKlinika/ZdravoKlinika/View/Manager/ViewModel/ManagerTransferEquipmentViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace ZdravoKlinika.View.Manager.ViewModel
 {
     public class ManagerTransferEquipmentViewModel : ManagerViewModelBase
     {
+        private RoomController roomController;
+        public ObservableCollection<Room> rooms;
+
+        public ObservableCollection<Room> Rooms { get; set; }
+
+        public ManagerTransferEquipmentViewModel()
+        {
+            this.roomController = new RoomController();
+            Rooms = new ObservableCollection<Room>(this.roomController.GetAll());
+        }
     }
 }
