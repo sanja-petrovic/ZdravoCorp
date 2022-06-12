3c59d7953d857a1abacf4d7824154564fd22783f
MiskLG <hornjak.ra139.2019@uns.ac.rs>
2022-06-11 15:51:34 -1000

    Added validation to add patient page

--- ZdravoKlinika/ZdravoKlinika/View/Secretary/SecretaryAddPatientPage.xaml.cs
+++ ZdravoKlinika/ZdravoKlinika/View/Secretary/SecretaryAddPatientPage.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ZdravoKlinika.View.Secretary.SecretaryViewModel;
 
 namespace ZdravoKlinika.View.Secretary
 {
@@ -24,6 +25,7 @@ namespace ZdravoKlinika.View.Secretary
         RegisteredPatientController registeredPatientController;
         public SecretaryAddPatientPage()
         {
+            DataContext = new AddPatientViewModel();
             InitializeComponent();
             registeredPatientController = new RegisteredPatientController();
         }
---
+++ ZdravoKlinika/ZdravoKlinika/View/Secretary/SecretaryViewModel/AddPatientViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZdravoKlinika.View.Secretary.SecretaryViewModel
+{
+    public  class AddPatientViewModel : ViewModelBase
+    {
+        private string name;
+        private string lastname;
+        private string pid;
+        private string email;
+        private string password;
+        private string phone;
+
+        public AddPatientViewModel() 
+        {
+        
+        }
+
+        public string Name { get => name; set => SetProperty(ref name, value); }
+        public string Lastname { get => lastname; set => SetProperty(ref lastname, value); }
+        public string Pid { get => pid; set => SetProperty(ref pid, value); }
+        public string Email { get => email; set => SetProperty(ref email, value);}
+        public string Password { get => password; set => SetProperty(ref password, value); }
+        public string Phone { get => phone; set => SetProperty(ref phone, value);}
+    }
+}
